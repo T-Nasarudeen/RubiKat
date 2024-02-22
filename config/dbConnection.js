bb5539fae088e9552e5c5968778415dd5b8755b7
Nasarudeen Thennadan <tnasarudeen@gmail.com>
2024-02-23 02:54:16 +0530

    add some changes on .env

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect(process.env.MONGOOSE_CONNECTION);
+    const conn = await mongoose.connect("mongodb+srv://tnasarudeen:uOPDHAJLx5ltP5Eb@cluster0.itopemf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("database connected");
   } catch (error) {
     console.log(error);
--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
-    secret: process.env.SECRET_KEY,
+    secret: "session secret key",
     resave: false,
     saveUninitialized: true,
     cookie:{
