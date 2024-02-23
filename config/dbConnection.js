b58af29f9743e9f7523c89477c2a51d265955a2f
Nasarudeen Thennadan <tnasarudeen@gmail.com>
2024-02-23 07:08:24 +0530

    changes on db connection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,9 +1,9 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
-require('dotenv').config()
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect("mongodb+srv://tnasarudeen:uOPDHAJLx5ltP5Eb@cluster0.itopemf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    const conn = await mongoose.connect(process.env.MONGOOSE_CONNECTION);
     console.log("database connected");
   } catch (error) {
     console.log(error);
